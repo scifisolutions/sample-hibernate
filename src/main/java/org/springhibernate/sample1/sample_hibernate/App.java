386c5e97838422ae71b0f84313c72568c7ec4d93
ajayakiwate <ajayakiwatesrp@gmail.com>
2022-04-13 18:04:01 +0530

    commited 5

--- src/main/java/org/springhibernate/sample1/sample_hibernate/App.java
+++ src/main/java/org/springhibernate/sample1/sample_hibernate/App.java
@@ -12,7 +12,6 @@ public class App
         
         System.out.println( "Hello World2!" );
         
-
         System.out.println( "Hello World4!" );
 
         System.out.println( "Hello World3!" );
