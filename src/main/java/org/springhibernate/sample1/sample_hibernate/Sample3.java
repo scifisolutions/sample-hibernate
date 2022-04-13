d11c5e9089e1c78a382b5c3e52559408b9b41fb3
ajayakiwate <ajayakiwatesrp@gmail.com>
2022-04-13 18:18:23 +0530

    Commit for Sample3

--- src/main/java/org/springhibernate/sample1/sample_hibernate/Sample3.java
+++ src/main/java/org/springhibernate/sample1/sample_hibernate/Sample3.java
@@ -3,6 +3,6 @@ package org.springhibernate.sample1.sample_hibernate;
 
 class Sample3{
 public static void main(String args[]){
-  System.out.println("Hello 2"); 
+  System.out.println("Hello 1"); 
 }
 }
