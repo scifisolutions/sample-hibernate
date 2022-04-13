5c52e5ef778aabce8cbfb02a0adf62e124e7eed6
ajayakiwate <ajayakiwatesrp@gmail.com>
2022-04-13 17:44:44 +0530

    First Commit

---
+++ src/main/java/org/springhibernate/sample1/sample_hibernate/App.java
@@ -0,0 +1,13 @@
+package org.springhibernate.sample1.sample_hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
