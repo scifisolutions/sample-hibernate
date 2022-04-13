e37821a44a63cb1f98a96ff8816e52b6d78cfa0c
ajayakiwate <ajayakiwatesrp@gmail.com>
2022-04-13 18:09:00 +0530

    Adding Sample 1

---
+++ src/main/java/org/springhibernate/sample1/sample_hibernate/Sample1.java
@@ -0,0 +1,10 @@
+package org.springhibernate.sample1.sample_hibernate;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println( "From Sample 1" );
+	}
+
+}
